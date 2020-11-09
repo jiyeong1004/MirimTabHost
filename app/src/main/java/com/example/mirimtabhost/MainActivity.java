611214ce154b8dc77e54f2fb6b74c3babe1999ec
jiyeong1004 <ak1157@naver.com>
2020-11-09 19:33:43 +0900

    Complete TabHost

--- app/src/main/java/com/example/mirimtabhost/MainActivity.java
+++ app/src/main/java/com/example/mirimtabhost/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.mirimtabhost;
 
+import android.app.TabActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends TabActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabHost tabHost = getTabHost();
+
+        TabHost.TabSpec tabSpecDog = tabHost.newTabSpec("Dog").setIndicator("강아지");
+        tabSpecDog.setContent(R.id.linear_dog);
+        tabHost.addTab(tabSpecDog);
+
+        TabHost.TabSpec tabSpecCat = tabHost.newTabSpec("Cat").setIndicator("고양이");
+        tabSpecCat.setContent(R.id.linear_cat);
+        tabHost.addTab(tabSpecCat);
+
+        TabHost.TabSpec tabSpecRabbit = tabHost.newTabSpec("Rabbit").setIndicator("토끼");
+        tabSpecRabbit.setContent(R.id.linear_rabbit);
+        tabHost.addTab(tabSpecRabbit);
+
+        tabHost.setCurrentTab(0);
     }
 }
